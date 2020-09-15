916b2b32d4829b18c7b37cf84834c5785450ee8f
MaxAndreevich <maxanokhin1997@gmail.com>
2020-09-16 00:16:26 +0300

    first commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Максим Разумов on 16.09.2020.
+//  Copyright © 2020 Максим Разумов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
