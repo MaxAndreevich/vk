5d6a27f6b32c743e0d35352bb0366ec6a2911ed4
MaxAndreevich <maxanokhin1997@gmail.com>
2020-09-24 21:59:08 +0300

    last bad commit

--- VK/Module/Profile/ProfilesPresenter.swift
+++ VK/Module/Profile/ProfilesPresenter.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class ProfilesPresenter {
+    
+    weak var viewController: ProfileViewController?
+    var profile: [Profile] = [Profile(userId: "Userid",
+                                      avatarURL: URL(string: "https://m.thepeoplesdialogue.org.za/Assets/images/about-herman.png"),
+                                      name: "Full name",
+                                      status: "Online or Offline",
+                                      thoughts: "Thought",
+                                      countFriend: 10,
+                                      city: "Moscow",
+                                      countSubscribers: 15,
+                                      placeWork: "Home",
+                                      placeLearn: "Sochi")]
+    
+    func getNumberOfRowsInSection(section: Int) -> Int {
+        return profile.count
+    }
+    
+    func getModelAtIndex(indexPath: IndexPath) -> Profile {
+
+        var profileModel = Profile(userId: profile[indexPath.row].userId,
+                                   avatarURL: profile[indexPath.row].avatarURL,
+                                   name: profile[indexPath.row].name,
+                                   status: profile[indexPath.row].status,
+                                   thoughts: profile[indexPath.row].thoughts,
+                                   countFriend: profile[indexPath.row].countFriend,
+                                   city: profile[indexPath.row].city,
+                                   countSubscribers: profile[indexPath.row].countSubscribers,
+                                   placeWork: profile[indexPath.row].placeWork,
+                                   placeLearn: profile[indexPath.row].placeLearn)
+        
+        return profileModel
+    }
+    
+}
